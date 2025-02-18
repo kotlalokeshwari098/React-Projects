99b6c97c6fae68a6df2e667c3666d316f4910d69
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-02-18 22:19:09 +0530

    bgchanger

---
+++ reactPro/src/components/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import data from './data'
+
+// single selection accoridian
+function Index() {
+    const [selected,setSelected]=useState(null)
+  return (
+    <div className='wrapper'>
+      <div className='accordian'>
+           {
+              data && data.length >0 ? 
+              data.map(dataItem => 
+                <div className="item">
+                    <div className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                </div>
+              )
+               :
+             <div> No data found</div>
+           }
+      </div>
+    </div>
+  )
+}
+
+export default Index
