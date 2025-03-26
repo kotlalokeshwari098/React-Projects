ef3888548fda9b44af55022bafe9fa9546ca7942
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-03-26 23:52:58 +0530

    main logic is added

--- MemoryGame/src/App.jsx
+++ MemoryGame/src/App.jsx
@@ -1,10 +1,113 @@
 import Card from "./components/Card";
 import './App.css'
+import React from "react";
+import { nanoid } from 'nanoid';
 
 export default function App(){
+  let [array,setArray]=React.useState(()=>generateCards())
+  const [flippedCards,setFlippedCards]=React.useState([]);
+  let [count,setCount]=React.useState(0)
+  // let flippedCards=[]
+
+
+  function generateCards(){
+    // let count=0;
+    let newArray=[] 
+    let items=['🦄',' 🌟',' 🐱',' 🚀','😍','🍬','👾',
+      '✈️','🦄',' 🌟',' 🐱',' 🚀','😍','🍬',
+      '👾','✈️'];
+
+    // implementing Fisher-Yates shuffle
+    for(let j=items.length-1;j>0;j--){
+      let random=Math.floor(Math.random() *(j+1));
+      [items[j],items[random]]=[items[random],items[j]];    
+    }
+    
+    for(let i=0;i<=15;i++){
+        newArray.push({
+          item:items[i],
+          key: nanoid(),
+          isHidden:false
+        })
+      
+      
+    }
+    return newArray; 
+  }
+
+
+  function showCard(id){
+    setArray(oldarray=>(
+      oldarray.map((item)=>{
+        if(item.key===id) 
+          return {...item,isHidden:!item.isHidden}
+        else return item
+      })
+    )) 
+    console.log(id)
+
+    if(flippedCards.length < 2){
+      
+      setFlippedCards(prevFlipped => [...prevFlipped, array.find(item => item.key === id)]);
+    }   
+  }
+  console.log("flipped cards:",flippedCards)
+  console.log(array);
+
+
+React.useEffect(()=>{
+  if(flippedCards.length===2){
+    if(flippedCards[0].item===flippedCards[1].item){
+      setArray(oldarray=>(
+        oldarray.map((items)=>{
+          if(items.item===flippedCards[0].item) 
+            return {...items,isHidden:true}
+          else return items
+        })
+      )) 
+    }
+    
+      else if(flippedCards[0].item !== flippedCards[1].item){
+        setTimeout(() => {
+        setArray(oldarray=>(
+          oldarray.map((item)=>{
+            if(item.key===flippedCards[0].key|| item.key===flippedCards[1].key) 
+              return {...item,isHidden:!item.isHidden}
+            else return item
+          })
+        )) 
+      }, 1000);
+      }
+    
+  setFlippedCards([])
+}
+},[flippedCards.length])
+
+
+function toggleButtons(){
+
+}
+    let cards=array.map((thing)=>(
+      <Card 
+      value={thing.item} 
+      isHidden={thing.isHidden}
+      showCard={()=>showCard(thing.key)}/>
+  ))
+      
+  
+ 
+ 
+  // console.log(cards)
   return(
     <div className="container">
-      <Card />
+      <div className="cards">
+
+        {cards}
+        
+      
+      </div>
+
+     
     </div>
   )
 }
--- MemoryGame/src/components/Card.jsx
+++ MemoryGame/src/components/Card.jsx
@@ -1,9 +1,11 @@
 import '../App.css'
 
-export default function Card(){
+export default function Card(prop){
     return(
-        <>
-
-        </>
+        <button className={prop.isHidden? 'showCard  card-container' : "hiddenCard  card-container"}
+        style={{backgroundColor:prop.isHidden ? 'black':'white'}}
+        onClick={prop.showCard}>
+               {prop.value}
+        </button>
     )
 }
