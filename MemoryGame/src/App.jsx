48fd60e434a0ef756583e2f147fca7c5440d1cc4
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-03-27 22:00:58 +0530

    initial project setup

--- MemoryGame/src/App.jsx
+++ MemoryGame/src/App.jsx
@@ -3,111 +3,110 @@ import './App.css'
 import React from "react";
 import { nanoid } from 'nanoid';
 
-export default function App(){
-  let [array,setArray]=React.useState(()=>generateCards())
-  const [flippedCards,setFlippedCards]=React.useState([]);
-  let [count,setCount]=React.useState(0)
-  // let flippedCards=[]
+export default function App() {
+  let [array, setArray] = React.useState(() => generateCards())
+  const [flippedCards, setFlippedCards] = React.useState([]);
+  const [isGameReset,setIsGameReset] = React.useState(false)
 
 
-  function generateCards(){
+  function generateCards() {
     // let count=0;
-    let newArray=[] 
-    let items=['🦄',' 🌟',' 🐱',' 🚀','😍','🍬','👾',
-      '✈️','🦄',' 🌟',' 🐱',' 🚀','😍','🍬',
-      '👾','✈️'];
+    let newArray = []
+    let items = ['🦄', ' 🌟', ' 🐱', ' 🚀', '😍', '🍬', '👾',
+      '✈️', '🦄', ' 🌟', ' 🐱', ' 🚀', '😍', '🍬',
+      '👾', '✈️'];
 
     // implementing Fisher-Yates shuffle
-    for(let j=items.length-1;j>0;j--){
-      let random=Math.floor(Math.random() *(j+1));
-      [items[j],items[random]]=[items[random],items[j]];    
+    for (let j = items.length - 1; j > 0; j--) {
+      let random = Math.floor(Math.random() * (j + 1));
+      [items[j], items[random]] = [items[random], items[j]];
     }
-    
-    for(let i=0;i<=15;i++){
-        newArray.push({
-          item:items[i],
-          key: nanoid(),
-          isHidden:false
-        })
-      
-      
+
+    for (let i = 0; i <= 15; i++) {
+      newArray.push({
+        item: items[i],
+        key: nanoid(),
+        isHidden: false
+      })
     }
-    return newArray; 
+    return newArray;
   }
 
 
-  function showCard(id){
-    setArray(oldarray=>(
-      oldarray.map((item)=>{
-        if(item.key===id) 
-          return {...item,isHidden:!item.isHidden}
+  function showCard(id) {
+    setArray(oldarray => (
+      oldarray.map((item) => {
+        if (item.key === id)
+          return { ...item, isHidden: !item.isHidden }
         else return item
       })
-    )) 
+    ))
     console.log(id)
 
-    if(flippedCards.length < 2){
-      
+    if (flippedCards.length < 2) {
+
       setFlippedCards(prevFlipped => [...prevFlipped, array.find(item => item.key === id)]);
-    }   
+    }
   }
-  console.log("flipped cards:",flippedCards)
+  console.log("flipped cards:", flippedCards)
   console.log(array);
 
 
-React.useEffect(()=>{
-  if(flippedCards.length===2){
-    if(flippedCards[0].item===flippedCards[1].item){
-      setArray(oldarray=>(
-        oldarray.map((items)=>{
-          if(items.item===flippedCards[0].item) 
-            return {...items,isHidden:true}
-          else return items
-        })
-      )) 
-    }
-    
-      else if(flippedCards[0].item !== flippedCards[1].item){
-        setTimeout(() => {
-        setArray(oldarray=>(
-          oldarray.map((item)=>{
-            if(item.key===flippedCards[0].key|| item.key===flippedCards[1].key) 
-              return {...item,isHidden:!item.isHidden}
-            else return item
+  React.useEffect(() => {
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].item === flippedCards[1].item) {
+        setArray(oldarray => (
+          oldarray.map((items) => {
+            if (items.item === flippedCards[0].item)
+              return { ...items, isHidden: true }
+            else return items
           })
-        )) 
-      }, 1000);
+        ))
       }
-    
-  setFlippedCards([])
-}
-},[flippedCards.length])
+
+      else if (flippedCards[0].item !== flippedCards[1].item) {
+        setTimeout(() => {
+          setArray(oldarray => (
+            oldarray.map((item) => {
+              if (item.key === flippedCards[0].key || item.key === flippedCards[1].key)
+                return { ...item, isHidden: !item.isHidden }
+              else return item
+            })
+          ))
+        }, 1000);
+      }
+
+      setFlippedCards([])
+    }
+  }, [flippedCards.length])
 
 
-function toggleButtons(){
+  function toggleButtons() {
+      if(array.every(item=>item.isHidden)) {
+        setIsGameReset(true);
+        setArray(generateCards())
 
-}
-    let cards=array.map((thing)=>(
-      <Card 
-      value={thing.item} 
+      }
+  }
+
+  let cards = array.map((thing) => (
+    <Card
+      value={thing.item}
       isHidden={thing.isHidden}
-      showCard={()=>showCard(thing.key)}/>
+      showCard={() => showCard(thing.key)} />
   ))
-      
-  
- 
- 
+
+
   // console.log(cards)
-  return(
+  return (
     <div className="container">
       <div className="cards">
 
         {cards}
-        
-      
+
       </div>
 
-     
+      <button className="main-button" onClick={toggleButtons}>{isGameReset ? 'Reset':'Start'}</button>
     </div>
   )
 }
