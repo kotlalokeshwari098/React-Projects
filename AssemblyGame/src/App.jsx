b1ecdb9d0567ec75aaf3a185831a90109d996264
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-03-29 22:36:09 +0530

    added more deatures

--- AssemblyGame/src/App.jsx
+++ AssemblyGame/src/App.jsx
@@ -2,29 +2,33 @@ import Header from "./components/Header";
 import { languages } from "./language";
 import React from "react";
 import clsx from "clsx";
+import { getFarewellText } from "./utils";
+import { getRandomWord } from "./utils";
 
 export default function App() {
 
   // state values
-  const [currentWord,setCurrentWord]=React.useState('react');
-  const [guessLetter,setGuessLetter]=React.useState([]);
-  
+  const [currentWord,setCurrentWord]=React.useState(()=>getRandomWord());
+  let [guessLetter,setGuessLetter]=React.useState([]);
+  console.log(currentWord)
 
   
+
   // derive from state 
+  const numGuessesLeft=languages.length-1;
   let wrongGuessCount=
         guessLetter.filter(letter => !currentWord.includes(letter)).length;
   
   const isGameWon=
       currentWord.split('').every(letter =>guessLetter.includes(letter));
-  const isGameLost=wrongGuessCount >-languages.length-1;
+  const isGameLost=wrongGuessCount >= languages.length-1;
   const isGameOver=isGameWon || isGameLost;
   
   
   // static values
   const alphabets='abcdefghijklmnopqrstuvwxyz'
   
-  
+  // console.log(getFarewellText())
 
   // languages depends on user guessing to kill or save
   const language =
@@ -48,6 +52,8 @@ export default function App() {
     const isGuessed =guessLetter.includes(letter);
     const isCorrect=isGuessed && currentWord.includes(letter)
     const isWrong=isGuessed && !currentWord.includes(letter)
+    let shouldRevelLetter=isGameLost || isGuessed
+    console.log(shouldRevelLetter)
     const className=clsx('letter-box',
       {
       show:isCorrect,
@@ -59,7 +65,7 @@ export default function App() {
     className={className}
     key={index}
     
-    >{letter.toUpperCase()}</span>
+    >{shouldRevelLetter ? letter.toUpperCase():''}</span>
   )
 })
 
@@ -77,7 +83,11 @@ let alphabetsLetters=alphabets.split('').map((letter)=>{
       <button 
       onClick={()=>storeGuessedLetters(letter)}
       className={className}
-      key={letter}>{letter.toUpperCase()}</button>
+      disabled={isGameOver}
+      aria-disabled={guessLetter.includes(letter)}
+      arial-label={`letter ${letter}`}
+      key={letter}>
+        {letter.toUpperCase()}</button>
     )
   })
   // console.log(className)
@@ -86,15 +96,25 @@ let alphabetsLetters=alphabets.split('').map((letter)=>{
   function storeGuessedLetters(letter){
     // console.log('hello')
     
-    setGuessLetter(prev => {
-      // prev.includes(letter) ? prev :[...prev,letter])
+    setGuessLetter(prev => (
+      prev.includes(letter) ? prev :[...prev,letter])
       // or
-      const letterSet=new Set(prev)
-      letterSet.add(letter);
-      return Array.from(letterSet)
-    })
+      // const letterSet=new Set(prev)
+      // letterSet.add(letter);
+      // return Array.from(letterSet)
+    )
   }
   console.log(guessLetter)
+
+  const lang=languages.map((item)=>item.name)
+  // console.log(lang)
+  
+  function resetGame(){
+    setCurrentWord(getRandomWord())
+    setGuessLetter([])
+
+  }
+
 
 
 
@@ -104,21 +124,53 @@ let alphabetsLetters=alphabets.split('').map((letter)=>{
         <h2>Assembly:Endgame</h2>
         <p>Guess the word in under 8 attempt to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="game-status">
-        <div>You win!</div>
-        <div>Well done🎉</div>
 
+      {
+        !isGameOver && 
+         <section className="game-status1">
+         { wrongGuessCount > 0 && getFarewellText(lang[wrongGuessCount-1])}
+         </section>
+      }
+
+      {
+        isGameOver && <section aria-live="polite" role="status" className="game-status">
+        {isGameWon && <div className="win-status">
+             <div>You win!</div>
+              <div>Well done🎉</div></div>}
+              
+        {isGameLost && <div className="lost-status">
+          <div>Game Over!</div>
+          <div>You lose! Better start learning Assembly😭</div>
+          </div>}
       </section>
+      }
+
       <section className="languageChips">
         {language}
       </section>
-      <section className="letter-blocks">
-        {letters}
+      
+       
+       <section className="letter-blocks">
+        {letters }
+        
       </section>
+
+       {/* combines visally-hidden aria-live region for status updates */}
+      <section className="sr-only" aria-live="polite" role='status'>
+        <p>
+          {currentWord.includes(guessLetter[guessLetter.length-1])?`correct!the letter ${guessLetter[guessLetter.length-1]} is in the word ` :`sorry the letter${guessLetter[guessLetter.length-1]} is not in the word`}
+          You have {numGuessesLeft}  attempts left
+        </p>
+        <p>Current word :{currentWord.split('').map(letter=> guessLetter.includes(letter ?letter +'.' : "blank"))}</p>
+
+      </section>
+
+      {/* keyboard */}
       <main >
          {alphabetsLetters}
       </main>
-     {isGameOver && <button className="decide-button">
+     {isGameOver && <button className="decide-button"
+     onClick={resetGame}>
          New Game
       </button>}
     </div>
