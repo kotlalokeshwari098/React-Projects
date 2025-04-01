b392b0a1d5d85ef60d8e6f4c6b8fd085bdb4de24
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-04-01 20:44:56 +0530

    added main logic

--- To-Do List/src/App.jsx
+++ To-Do List/src/App.jsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
+import {clsx} from 'clsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [value, setValue] = useState('')
+  const [tasks, setTasks] = useState([]);
+  const [completed,setCompleted]=useState(0)
+
+
+  function renderTask() {
+    setTasks(prev => ([
+      ...prev,
+      {
+        value: value,
+        key: nanoid(),
+        active :false 
+      }
+    ]))
+
+    setValue('')
+  }
+
+
+  function deleteTask(id) {
+    setTasks(prev => (
+      prev.filter(item => item.key !== id)
+    ))
+  }
+
+  function toggleTask(id){
+    setTasks((prev) => prev.map((item)=>
+      {
+      if(item.key===id) return {...item,active:!item.active}
+      else return item
+    }
+  ))
+  setCompleted(((tasks.filter(item => item.active)).length)+1)
+  }
+
+ 
+
+
+  let task =
+    tasks.map((item) => (
+      <div className="box" key={item.key}>
+        <div className="box-left">
+          <div 
+          
+          className= {item.active ? 'color-rep color-full':'color-rep'}
+          onClick={()=>toggleTask(item.key)}>
+
+          </div>
+          <div className="task-name" >
+            {item.value}
+          </div>
+        </div>
+        <div className="box-right">
+          <div className="rename">
+            <span className="material-symbols-outlined">
+              edit_square
+            </span>
+          </div>
+          <div className="delete" onClick={() => deleteTask(item.key)}>
+            <span className="material-symbols-outlined">
+              delete
+            </span>
+          </div>
+        </div>
+      </div>
+
+    )
+    )
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="score-card">
+        <div className="motivate-talk">
+          <p>Task done keep it up</p>
+        </div>
+        <div className="score">
+          <div className="circular-board">
+          {completed}/{tasks.length}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="input-task">
+        <div className="input">
+          <input
+            type="text"
+            placeholder='Write your next task'
+            value={value}
+            onChange={(e) => setValue(e.target.value)} />
+        </div>
+        <div className="add-symbol">
+          <div className="circular-symbol"
+            onClick={renderTask}>
+            +
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="tasks-container">
+        {task}
+
+      </div>
+    </div>
   )
 }
-
-export default App
